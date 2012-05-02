4e22ba85caed9b2b1a7e5489326692e106f07e29
Damon Oehlman <damon.oehlman@sidelab.com>
2012-05-02 19:48:19 +1000

    Updated to use nopt for passing command line args

--- index.js
+++
@@ -1,9 +0,0 @@
-var Boxer = require('./lib/boxer'),
-    Processor = require('./lib/processor');
-    
-exports = module.exports = function(target) {
-    return new Boxer(target);
-};
-
-exports.Boxer = Boxer;
-exports.Processor = Processor;
